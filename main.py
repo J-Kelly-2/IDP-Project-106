f18a47e3f65e9a5edae80db66791fc2b9d019d59
Hassan Himaz <hh609@cam.ac.uk>
2025-01-28 17:49:22 +0000

    modified:   main.py

--- main.py
+++ main.py
@@ -15,8 +15,24 @@ frls = LineSensor()
 def find_type_of_line(): #tells us if we are on a line, veering off, at a junction(left,right,T)
     if flls == 0 and lls == 0 and rls == 0 and frls == 0:
         return 'OFFTHEGRID'
+    
     #then other logic that turns the bot
+
+def move_forward():
+    pass
+
+def turn(direction):
+    pass
+
+def adjust(direction):
+    pass
 #main loop:
 while True:
-    if find_type_of_line() == 'OFFTHEGRID':
-        break
+    state = find_type_of_line() #where robot is
+    match state:
+        case 'OFFTHEGRID':
+            break
+        case 'LINE':
+            move_forward()
+        case 'RIGHTOFFLINE':
+            adjust('right')
